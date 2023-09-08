47dac70d0f899b8a4dba32a742155a6301972046
Daniel <danielseniv@outlook.com>
2023-09-07 23:45:44 -0400

    Add folder icon component

---
+++ client/src/components/FolderIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./FolderIcon.module.scss"
+
+const FolderIcon = () => {
+    return (
+        <div className={styles.allFolders}>
+            <img src="/icons/icons8-folder-160.png" className={StyleSheet.folderIcons} />
+        </div>
+    )
+}
+
+export default FolderIcon
