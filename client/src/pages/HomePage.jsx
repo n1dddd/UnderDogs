26c6878ff527fbdcf6fa38897e83a75229637432
Daniel <danielseniv@outlook.com>
2023-09-11 01:04:34 -0400

    Change location of logo for bg img

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import styles from "./HomePage.module.scss"
 import FolderIcons from '../components/FolderIcon'
+import happyFace from '../assets/highQualityHappy.png'
 
 const HomePage = () => {
     return (
         <div className={styles.HomePageContainer}>
+            <img src={happyFace} className={styles.bgImg} />
             <FolderIcons />
         </div>
     )
