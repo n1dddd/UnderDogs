c2a41c45c718ce7bef516fbd0464ade643523450
Daniel <danielseniv@outlook.com>
2023-09-10 23:52:23 -0400

    Add draggable folder component, check if div is clicked for later use

--- client/src/components/FolderIcon.jsx
+++ client/src/components/FolderIcon.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import Draggable from "react-draggable";
 import styles from "./FolderIcon.module.scss"
+import openedFolder from "/icons/icons8-opened-folder-160.png"
+import closedFolder from "/icons/icons8-folder-160.png"
 
 const FolderIcon = () => {
+    const didClick = (e) => {
+        e.preventDefault();
+        console.log("Element was clicked")
+    }
     return (
         <div className={styles.allFolders}>
-            <div className={styles.productContainer}>
-                <img src="/icons/icons8-folder-160.png" className={styles.folderIcon} />
-                <p className={styles.folderText}>mayze #dizzy newjazz (loopkit + midi & flps) [$14.98]</p>
-            </div>
-        </div>
+            <Draggable>
+                <div className={styles.productContainer} onClick={didClick}>
+                    <img src={closedFolder} onMouseOver={e => (e.currentTarget.src = openedFolder)} onMouseOut={e => (e.currentTarget.src = closedFolder)} className={styles.folderIcon} />
+                    <p>mayze #dizzy newjazz (loopkit + midi & flps) [$14.98]</p>
+                </div>
+            </Draggable>
+        </div >
     )
 }
 
