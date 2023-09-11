4c5d25df10145842550883d429f20fd06bbd97c1
Daniel <danielseniv@outlook.com>
2023-09-11 01:02:35 -0400

    Check for native double click handler -- important to open modal

--- client/src/components/FolderIcon.jsx
+++ client/src/components/FolderIcon.jsx
@@ -5,14 +5,14 @@ import openedFolder from "/icons/icons8-opened-folder-160.png"
 import closedFolder from "/icons/icons8-folder-160.png"
 
 const FolderIcon = () => {
-    const didClick = (e) => {
+    const didDoubleClick = (e) => {
         e.preventDefault();
-        console.log("Element was clicked")
+        console.log("Element was double clicked")
     }
     return (
         <div className={styles.allFolders}>
             <Draggable>
-                <div className={styles.productContainer} onClick={didClick}>
+                <div className={styles.productContainer} onDoubleClick={didDoubleClick}>
                     <img src={closedFolder} onMouseOver={e => (e.currentTarget.src = openedFolder)} onMouseOut={e => (e.currentTarget.src = closedFolder)} className={styles.folderIcon} />
                     <p>mayze #dizzy newjazz (loopkit + midi & flps) [$14.98]</p>
                 </div>
