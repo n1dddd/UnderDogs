46f5e541bf83901c4d110f9721dd5523edad05c9
Daniel <danielseniv@outlook.com>
2023-09-12 01:20:49 -0400

    Add updated folder design:

--- client/src/components/FolderIcon.jsx
+++ client/src/components/FolderIcon.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useFinderStore } from '../stores/finderStore';
 import Draggable from "react-draggable";
 import styles from "./FolderIcon.module.scss"
-import openedFolder from "/icons/openFolder.png"
 import closedFolder from "/icons/closedFolder.png"
 import FinderComponent from './FinderComponent';
 
@@ -20,7 +19,7 @@ const FolderIcon = () => {
         <div className={styles.allFolders}>
             <Draggable cancel={styles.productContainer}>
                 <div className={styles.productContainer} onDoubleClick={() => setFinderOpen()} onTouchStart={() => setFinderOpen()}>
-                    <img src={closedFolder} onMouseOver={e => (e.currentTarget.src = openedFolder)} onMouseOut={e => (e.currentTarget.src = closedFolder)} className={styles.folderIcon} />
+                    <img src={closedFolder} className={styles.folderIcon} />
                     <p>mayze #dizzy newjazz (loopkit + midi & flps) [$14.98]</p>
                 </div>
             </Draggable>
