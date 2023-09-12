1e69b5c7421a06bae1338f1b3ed5b7225710ae73
Daniel <danielseniv@outlook.com>
2023-09-12 00:01:51 -0400

    Add FinderComponent modal, add intial styling and structure

---
+++ client/src/components/FinderComponent.jsx
@@ -0,0 +1,21 @@
+import { useFinderStore } from "../stores/finderStore"
+import styles from "./FinderComponent.module.scss";
+import Draggable from "react-draggable";
+import circle from "../assets/red_circle.png"
+
+const FinderComponent = () => {
+    const setFinderClose = useFinderStore((state) => state.closeFinder)
+    return (
+        <Draggable>
+            <div className={styles.finderCard}>
+                <div className={styles.finderCardHeader}>
+                    <img src={circle} className={styles.exitCircle} onClick={() => setFinderClose()} />
+                    <p className={styles.categoriesText}>Categories</p>
+                </div>
+                <div className={styles.finderCardBody}></div>
+            </div>
+        </Draggable>
+    )
+}
+
+export default FinderComponent
