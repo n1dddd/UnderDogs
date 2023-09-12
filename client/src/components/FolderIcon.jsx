70c0f1460810e7e3033a98e7c23117a9865fbf96
Daniel <danielseniv@outlook.com>
2023-09-11 23:47:51 -0400

    Add conditional rendering to display early thoughts of finder design

--- client/src/components/FolderIcon.jsx
+++ client/src/components/FolderIcon.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
+import { useFinderStore } from '../stores/finderStore';
 import Draggable from "react-draggable";
 import styles from "./FolderIcon.module.scss"
 import openedFolder from "/icons/openFolder.png"
 import closedFolder from "/icons/closedFolder.png"
+import FinderComponent from './FinderComponent';
 
 const FolderIcon = () => {
-    const didDoubleClick = (e) => {
-        e.preventDefault();
-        console.log("Element was double clicked")
+    const setFinderOpen = useFinderStore((state) => state.openFinder)
+    const finder = useFinderStore((state) => state.isFinderOpen)
+    console.log(useFinderStore((state) => state.isFinderOpen))
+
+    if (finder) {
+        return (
+            <FinderComponent />
+        )
     }
     return (
         <div className={styles.allFolders}>
             <Draggable>
-                <div className={styles.productContainer} onDoubleClick={didDoubleClick}>
+                <div className={styles.productContainer} onDoubleClick={() => setFinderOpen()}>
                     <img src={closedFolder} onMouseOver={e => (e.currentTarget.src = openedFolder)} onMouseOut={e => (e.currentTarget.src = closedFolder)} className={styles.folderIcon} />
                     <p>mayze #dizzy newjazz (loopkit + midi & flps) [$14.98]</p>
                 </div>
