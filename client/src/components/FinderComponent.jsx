a18a0e2de0e1d1be8ec2a45e2a95f585e86872fb
Daniel <danielseniv@outlook.com>
2023-09-13 01:55:32 -0400

    Add FinderComponentCategories component sidebar

--- client/src/components/FinderComponent.jsx
+++ client/src/components/FinderComponent.jsx
@@ -1,16 +1,18 @@
 import { useFinderStore } from "../stores/finderStore"
 import styles from "./FinderComponent.module.scss";
 import Draggable from "react-draggable";
-import circle from "../assets/red_circle.png"
+import circle from "../assets/red_circle.png";
+import FinderComponentCategories from './FinderComponentCategories'
 
 const FinderComponent = () => {
     const setFinderClose = useFinderStore((state) => state.closeFinder)
     return (
         <Draggable>
             <div className={styles.finderCard}>
-                <div className={styles.finderCardHeader}>
+                <div className={styles.finderCardSideBar}>
                     <img src={circle} className={styles.exitCircle} onClick={() => setFinderClose()} onTouchStart={() => setFinderClose()} />
-                    <p className={styles.categoriesText}>Categories</p>
+                    <h2 className={styles.categoriesText}>Categories</h2>
+                    <FinderComponentCategories />
                 </div>
                 <div className={styles.finderCardBody}></div>
             </div>
