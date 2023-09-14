ff65976c6af39162e096bd1c6e8fc5fd2419ac10
Daniel <danielseniv@outlook.com>
2023-09-13 23:30:32 -0400

    Dynamically render categories from state, state data retrieved from firestore

--- client/src/components/FinderComponentCategories.jsx
+++ client/src/components/FinderComponentCategories.jsx
@@ -1,30 +1,16 @@
 import React from 'react'
-import { useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../config/firebase.js"
-import { useCategoriesStore } from '../stores/categoriesStore.js';
 import styles from "./FinderComponentCategories.module.scss"
+import { useCategoriesStore } from '../stores/categoriesStore.js';
 
 const FinderComponentCategories = () => {
 
     const setActiveCategory = useCategoriesStore((state) => state.setActiveCategory);
-    const setCategories = useCategoriesStore((state) => state.setCategories)
     const statefulCategories = useCategoriesStore((state) => state.categories)
-    const getCategoryInformation = async () => {
-        const querySnapshot = await getDocs(collection(db, "shop"));
-        const storeCategories = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
-        console.log(storeCategories[0].category)
-        setCategories(storeCategories[0].category);
-    }
-    console.log(statefulCategories)
 
-    useEffect(() => {
-        getCategoryInformation();
-    }, [])
 
     const productCategories = statefulCategories.map((category, index) => {
         return (
-            <div key={index} className={styles.categoryName}>{category}</div>
+            <h1 key={index} className={styles.categoryName}>{category}</h1>
         )
     })
     return (
