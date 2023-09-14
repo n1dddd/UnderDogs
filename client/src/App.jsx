df4eb181367b4e5c3db53aced33ddf92016875ec
Daniel <danielseniv@outlook.com>
2023-09-13 23:27:31 -0400

    Use as temporary component to make API call to firestore, stores call in state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,32 @@
 import HomePage from './pages/HomePage'
 import "./scss/styles.scss"
-
+import { useCategoriesStore } from './stores/categoriesStore.js';
+import { useProductsStore } from "./stores/productsStore.js";
+import { useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./config/firebase.js"
 
 
 function App() {
+  const setCategories = useCategoriesStore((state) => state.setCategories)
+  const setProducts = useProductsStore((state) => state.setProducts)
+  const getCategoryInformation = async () => {
+    const querySnapshot = await getDocs(collection(db, "shop"));
+    const storeCategories = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+    setCategories(storeCategories[0].category);
+  }
+  const getProductInformation = async () => {
+    const querySnapshot = await getDocs(collection(db, "products"));
+    const products = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+    console.log(products[0].img)
+    setProducts(products)
+  }
+
+  useEffect(() => {
+    getCategoryInformation();
+    getProductInformation();
+  }, [])
+
   return (
     <HomePage />
   )
