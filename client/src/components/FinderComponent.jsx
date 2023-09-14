d824ba16619706b59b92bf179be5980f650be665
Daniel <danielseniv@outlook.com>
2023-09-13 23:29:03 -0400

    Change P tags, add className to h1, add styling to finderCardBody

--- client/src/components/FinderComponent.jsx
+++ client/src/components/FinderComponent.jsx
@@ -1,20 +1,25 @@
 import { useFinderStore } from "../stores/finderStore"
+
 import styles from "./FinderComponent.module.scss";
 import Draggable from "react-draggable";
 import circle from "../assets/red_circle.png";
 import FinderComponentCategories from './FinderComponentCategories'
+import ProductComponent from "./ProductComponent";
 
 const FinderComponent = () => {
     const setFinderClose = useFinderStore((state) => state.closeFinder)
+
     return (
         <Draggable>
             <div className={styles.finderCard}>
                 <div className={styles.finderCardSideBar}>
                     <img src={circle} className={styles.exitCircle} onClick={() => setFinderClose()} onTouchStart={() => setFinderClose()} />
-                    <h2 className={styles.categoriesText}>Categories</h2>
+                    <h1 className={styles.categoriesText}>Categories</h1>
                     <FinderComponentCategories />
                 </div>
-                <div className={styles.finderCardBody}></div>
+                <div className={styles.finderCardBody}>
+                    <ProductComponent />
+                </div>
             </div>
         </Draggable>
     )
