bf7716c8ecf6cefcad3debbc68eecd6dc4bcec2b
Daniel <danielseniv@outlook.com>
2023-09-15 01:12:55 -0400

    Add a filtered products state and setter function

--- client/src/stores/productsStore.js
+++ client/src/stores/productsStore.js
@@ -2,7 +2,9 @@ import { create } from "zustand";
 
 export const useProductsStore = create((set) => ({
     products: [],
+    filteredProducts: [],
     activeProduct: "",
     setProducts: (dbProducts) => set({ products: dbProducts }),
-    setActiveProduct: (selectedProduct) => set({ activeProduct: selectedProduct })
+    setActiveProduct: (selectedProduct) => set({ activeProduct: selectedProduct }),
+    setFilteredProducts: (filteredState) => set({ filteredProducts: filteredState })
 }))
