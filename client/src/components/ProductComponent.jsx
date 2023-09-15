d2611c0032498cb690cef84eea48c64e2b536070
Daniel <danielseniv@outlook.com>
2023-09-15 01:12:02 -0400

    Add a filter of products based on active category, render callback (state update) when activeCategory state changes

--- client/src/components/ProductComponent.jsx
+++ client/src/components/ProductComponent.jsx
@@ -1,8 +1,25 @@
+import { useEffect } from "react";
 import { useProductsStore } from "../stores/productsStore";
+import { useCategoriesStore } from "../stores/categoriesStore";
 import styles from "./ProductComponent.module.scss";
 
 const ProductComponent = () => {
-    const statefulProducts = useProductsStore((state) => state.products);
+    const statefulProducts = useProductsStore((state) => state.filteredProducts);
+    const activeCategory = useCategoriesStore((state) => state.activeCategory);
+    const products = useProductsStore((state) => state.products);
+    const setFilteredProducts = useProductsStore((state) => state.setFilteredProducts);
+
+    useEffect(() => {
+        const getFilteredProducts = () => {
+            let filteredProducts = products.filter((product) => {
+                return product.category === activeCategory;
+            })
+            console.log(filteredProducts)
+            setFilteredProducts(filteredProducts)
+        };
+        getFilteredProducts();
+    }, [activeCategory])
+
     const fileComponent = statefulProducts.map((product, index) => {
         return (
 
