9680420a8817a02b2e32aadf47bddfe886d903a7
Daniel <danielseniv@outlook.com>
2023-09-14 23:53:53 -0400

    Add rudmentary product filter function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ import { db } from "./config/firebase.js"
 function App() {
   const setCategories = useCategoriesStore((state) => state.setCategories)
   const setProducts = useProductsStore((state) => state.setProducts)
+  const activeCategory = useCategoriesStore((state) => state.activeCategory);
+  const products = useProductsStore((state) => state.products);
   const getCategoryInformation = async () => {
     const querySnapshot = await getDocs(collection(db, "shop"));
     const storeCategories = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
@@ -18,9 +20,27 @@ function App() {
   const getProductInformation = async () => {
     const querySnapshot = await getDocs(collection(db, "products"));
     const products = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
-    console.log(products[0].img)
+    console.log(products);
     setProducts(products)
   }
+  const getFilteredProducts = () => {
+    let filteredProducts = products.reduce(function (result, option) {
+      if (activeCategory) {
+        if (option.category === activeCategory) {
+          let filteredProductsByCategory = { ...option };
+          result.push(filteredProductsByCategory);
+        }
+      }
+      console.log(result);
+      return result;
+    }, [])
+    setProducts(filteredProducts)
+  };
+
+
+  useEffect(() => {
+    getFilteredProducts();
+  }, [activeCategory])
 
   useEffect(() => {
     getCategoryInformation();
