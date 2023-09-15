16e79ea034f38ee95ab92a152c66a27577fc685b
Daniel <danielseniv@outlook.com>
2023-09-15 01:00:14 -0400

    Add category filtering/active state of selected category

--- client/src/components/FinderComponentCategories.jsx
+++ client/src/components/FinderComponentCategories.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import { useEffect } from 'react'
 import styles from "./FinderComponentCategories.module.scss"
 import { useCategoriesStore } from '../stores/categoriesStore.js';
+import { useProductsStore } from '../stores/productsStore';
 
 const FinderComponentCategories = () => {
-
-    const setActiveCategory = useCategoriesStore((state) => state.setActiveCategory);
-    const statefulCategories = useCategoriesStore((state) => state.categories)
-
-
+    const setActiveCategory = useCategoriesStore((state) => state.setActiveCategory)
+    const statefulCategories = useCategoriesStore((state) => state.categories);
+    const activeCategory = useCategoriesStore((state) => state.activeCategory);
     const productCategories = statefulCategories.map((category, index) => {
-        return (
-            <h1 key={index} className={styles.categoryName}>{category}</h1>
-        )
+        if (activeCategory === category) {
+            return (
+                <h1 key={index} className={styles.categoryName__active} onClick={() => setActiveCategory(category)}>{category}</h1>
+            )
+
+        } else {
+            return (
+                <h1 key={index} className={styles.categoryName} onClick={() => setActiveCategory(category)}>{category}</h1>
+            )
+        }
     })
     return (
         <>{productCategories}</>
