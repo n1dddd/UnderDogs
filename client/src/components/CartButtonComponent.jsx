09d8eba84bb3d7c85d7cd4cc9b5911c876c2f1f4
Daniel <danielseniv@outlook.com>
2023-09-18 02:34:25 -0400

    Add component that when clicked, add product to cart

---
+++ client/src/components/CartButtonComponent.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react'
+import daBag from "../assets/dabag.png"
+import styles from "./CartButtonComponent.module.scss"
+import { useCartStore } from '../stores/cartStore'
+
+
+const CartButtonComponent = (product) => {
+
+    const addToCart = useCartStore((state) => state.addToCart)
+    const inBag = useCartStore((state) => state.cart);
+    return (
+        <div onClick={() => addToCart(product)}>
+            <img src={daBag} className={styles.addToCartIcon} onClick={() => console.log(inBag)} />
+        </div>
+    )
+}
+
+export default CartButtonComponent
