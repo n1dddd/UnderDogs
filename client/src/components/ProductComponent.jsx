e2f2cdafee2d0bc8ffc2029c7fb87d20f31bb1af
Daniel <danielseniv@outlook.com>
2023-09-18 02:33:06 -0400

    Add CartButtonComponent to each product

--- client/src/components/ProductComponent.jsx
+++ client/src/components/ProductComponent.jsx
@@ -2,22 +2,19 @@ import { useEffect } from "react";
 import { useProductsStore } from "../stores/productsStore";
 import { useCategoriesStore } from "../stores/categoriesStore";
 import styles from "./ProductComponent.module.scss";
+import CartButtonComponent from "./CartButtonComponent";
 
 const ProductComponent = () => {
     const statefulProducts = useProductsStore((state) => state.filteredProducts);
     const activeCategory = useCategoriesStore((state) => state.activeCategory);
     const products = useProductsStore((state) => state.products);
     const setFilteredProducts = useProductsStore((state) => state.setFilteredProducts);
-
-    console.log(products);
-
     useEffect(() => {
         const getFilteredProducts = () => {
             let filteredProducts = products.filter((product) => {
                 return product.stripe_metadata_category
                     === activeCategory || product.category === activeCategory;
             })
-            console.log(filteredProducts)
             setFilteredProducts(filteredProducts)
         };
         getFilteredProducts();
@@ -30,6 +27,7 @@ const ProductComponent = () => {
                 <img className={styles.productFileIcon} src={product.images[0]} />
                 <p className={styles.productName}>{product.name}</p>
                 <p className={styles.productPrice}>${product.unit_amount / 100}</p>
+                <CartButtonComponent product={product} />
             </div>
 
         )
@@ -38,6 +36,7 @@ const ProductComponent = () => {
         <>
             <div className={styles.allProductsContainer}>
                 {fileComponent}
+
             </div >
         </>
     )
