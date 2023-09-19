66ba5c5b68741ac1475f2ead08710254d0b2b140
Daniel <danielseniv@outlook.com>
2023-09-18 23:14:15 -0400

    Add number next to cart category name to denote # of products in cart

--- client/src/components/CartSideBar.jsx
+++ client/src/components/CartSideBar.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import { useCategoriesStore } from '../stores/categoriesStore'
 import styles from "./CartSideBar.module.scss";
+import { useCartStore } from '../stores/cartStore';
 
 const CartSideBar = () => {
     const activeCategory = useCategoriesStore((state) => state.activeCategory);
     const setActiveCategory = useCategoriesStore((state) => state.setActiveCategory)
+    const statefulCart = useCartStore((state) => state.cart)
     const activeCategoryHeader = () => {
         if (activeCategory === "cart") {
             return (
-                <h1 className={styles.cartHeader__active}>Cart</h1>
+                <h1 className={styles.cartHeader__active}>Cart ({statefulCart.length})</h1>
             )
         }
         else {
             return (
-                <h1 onClick={() => setActiveCategory("cart")} className={styles.cartHeader}>Cart</h1>
+                <h1 onClick={() => setActiveCategory("cart")} onTouchStart={() => setActiveCategory("cart")} className={styles.cartHeader}>Cart ({statefulCart.length})</h1>
             )
         }
 
