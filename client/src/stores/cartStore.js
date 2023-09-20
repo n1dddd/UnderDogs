81e3dde0e972e01722f0d35750b48d0f46da19c0
Daniel <danielseniv@outlook.com>
2023-09-19 23:54:41 -0400

    Add remove from cart function to cart state store

--- client/src/stores/cartStore.js
+++ client/src/stores/cartStore.js
@@ -3,5 +3,11 @@ import { create } from "zustand";
 export const useCartStore = create((set) => ({
     cart: [],
     cartView: false,
-    addToCart: (product) => set((state) => ({ cart: [...state.cart, product] })),
+    addToCart: (product) => set((state) => (
+        { cart: [...state.cart, product] })),
+    removeFromCart: (e) => set((state) => ({
+        cart: [...state.cart.filter(function (product) {
+            return product != e;
+        })]
+    }))
 }))
