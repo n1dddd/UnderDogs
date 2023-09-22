0a93484b2582c2fcbfbc94352a462df372c45726
Daniel <danielseniv@outlook.com>
2023-09-21 23:52:36 -0400

    Add onTouchStart handler to register touch events for X symbol -- remove from cart

--- client/src/components/CartComponent.jsx
+++ client/src/components/CartComponent.jsx
@@ -6,14 +6,13 @@ import styles from "./CartComponent.module.scss"
 const CartComponent = () => {
     const statefulCart = useCartStore((state) => state.cart);
     const removeFromCart = useCartStore((state) => state.removeFromCart);
-
     const productsInCart = statefulCart.map((product, index) => {
         return (
             <div key={index} className={styles.checkoutContainer}>
                 <img className={styles.productFileIcon} src={product.product.images[0]} />
                 <p className={styles.productName}>{product.product.name}</p>
                 <p className={styles.productPrice}>${product.product.unit_amount / 100}</p>
-                <img className={styles.removeX} src={XSymbol} onClick={() => removeFromCart(product)} />
+                <img className={styles.removeX} src={XSymbol} onClick={() => removeFromCart(product)} onTouchStart={() => removeFromCart(product)} />
             </div>
         )
     })
