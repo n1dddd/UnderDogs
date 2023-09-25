2a9ad6e793d70d8b353572afd45beab9eaa69b1c
Daniel <danielseniv@outlook.com>
2023-09-25 00:46:58 -0400

    Give the sign-in page the same skeleton as as the sign-up page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from "./SignIn.module.scss"
+import { Link } from 'react-router-dom'
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSubmit = async () => {
+
+    }
+    return (
+        <div className={styles.authCard}>
+            <h1 className={styles.authCardHeader}>Login</h1>
+            <form className={styles.authForm} onSubmit={handleSubmit}>
+                <div className={styles.emailPassInput}>
+                    <input onChange={(e) => setEmail(e.target.value)} type="email" className={styles.inputField}></input>
+                    <label>
+                        Email Address
+                    </label>
+                </div>
+                <div className={styles.emailPassInput}>
+                    <input type="password" className={styles.inputField} onChange={(e) => setPassword(e.target.value)}></input>
+                    <label>
+                        Password
+                    </label>
+                </div>
+                <button className={styles.authButton}>Login</button>
+            </form>
+            <p className={styles.authRoute}>Don't have an account? <Link to="/sign_up" className={styles.linkUnderline} >Sign Up.</Link></p>
+        </div>
+    )
 }
 
 export default SignIn
