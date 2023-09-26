6ec6bad27c0ddbe1d93129f45f5d98f51451edd6
Daniel <danielseniv@outlook.com>
2023-09-25 23:32:49 -0400

    Add login and logout context

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
 import { auth } from "../config/firebase";
 import {
     createUserWithEmailAndPassword,
@@ -7,16 +7,41 @@ import {
     onAuthStateChanged,
     getAuth
 } from "firebase/auth"
+import { useUserStore } from "../stores/userStore"
 
 const UserContext = createContext();
 
-export const AuthContextProvider = ({ children }) => {
 
+
+export const AuthContextProvider = ({ children }) => {
+    const authUser = useUserStore((state) => state.user);
+    const setAuthUser = useUserStore((state) => state.setUser)
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
     }
+
+    const login = async (email, password) => {
+        return await signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => { setAuthUser(userCredential.user) })
+    }
+
+    const logout = () => {
+        setAuthUser({});
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log(currentUser);
+            setAuthUser(currentUser);
+            console.log(authUser);
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
     return (
-        <UserContext.Provider value={{ createUser }}>
+        <UserContext.Provider value={{ createUser, authUser, logout, login }}>
             {children}
         </UserContext.Provider>
     )
