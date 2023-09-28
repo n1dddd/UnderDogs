6d5230f58c79264af9698a57d9fc5f5c2d8ba8fa
Daniel <danielseniv@outlook.com>
2023-09-28 01:07:20 -0400

    Add getSubtotal function, formatedCheckout function, and feed stripe API function correct data

--- client/src/components/CheckoutBar.jsx
+++ client/src/components/CheckoutBar.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 import styles from "./CheckoutBar.module.scss"
+import getStripe from '../../lib/getStripe'
+import { useCartStore } from '../stores/cartStore';
 
 const CheckoutBar = () => {
+
+    const statefulCart = useCartStore((state) => state.cart)
+
+    const getSubtotal = () => {
+        const finalCart = [...statefulCart]
+        let subTotalInCents = 0;
+        console.log(finalCart);
+        finalCart.forEach((product) => {
+            subTotalInCents += product.product.unit_amount;
+        })
+        return subTotalInCents / 100
+    }
+
+    const formatedCheckout = () => {
+        const finalCart = [...statefulCart]
+        console.log(finalCart);
+        return finalCart.map((product) => {
+            return {
+                price: product.product.priceId,
+                quantity: 1
+            }
+        })
+    }
+
+    const handleCheckout = async () => {
+        let cart = formatedCheckout();
+        const stripe = await getStripe();
+        const { error } = await stripe.redirectToCheckout({
+            lineItems: cart,
+            mode: 'payment',
+            successUrl: "http://localhost:5173/",
+            cancelUrl: "http://localhost:5173/"
+        });
+        console.log(error.message);
+    }
     return (
-        <div className={styles.checkoutBar}>CheckoutBar</div>
+        <div className={styles.checkoutBar}>
+            <p className={styles.subTotalText}>Subtotal: CAD ${getSubtotal()}</p>
+            <button onClick={() => handleCheckout()}>Check out</button>
+        </div>
     )
 }
 
