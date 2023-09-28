c5100297f345733d53cd1e989ea10705a8c10855
Daniel <danielseniv@outlook.com>
2023-09-27 23:55:38 -0400

    Add styling to checkout bar

--- client/src/components/CheckoutBar.jsx
+++ client/src/components/CheckoutBar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from "./CheckoutBar.module.scss"
 
 const CheckoutBar = () => {
-  return (
-    <div>CheckoutBar</div>
-  )
+    return (
+        <div className={styles.checkoutBar}>CheckoutBar</div>
+    )
 }
 
 export default CheckoutBar
