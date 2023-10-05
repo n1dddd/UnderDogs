d7591f5f46450046d6506a95e124a5287452ee19
Daniel <danielseniv@outlook.com>
2023-10-04 22:22:01 -0400

    Add dynamic URL routing for products

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import { Routes, Route } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import SignUp from "./pages/SignUp";
 import { AuthContextProvider } from "../src/context/AuthContext"
+import ProductPage from './pages/ProductPage';
 
 
 function App() {
@@ -51,6 +52,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<SignIn />} />
         <Route path="/sign_up" element={<SignUp />} />
+        <Route path="/products/:id" element={<ProductPage />} />
       </Routes>
     </AuthContextProvider>
   )
