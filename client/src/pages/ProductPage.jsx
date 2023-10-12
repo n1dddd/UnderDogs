e45165d04c7764dfb40ed3a141e30cdf87b6aaff
Daniel <danielseniv@outlook.com>
2023-10-11 22:37:40 -0400

    Re-change to API call to pull product information and product pricing

--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { Suspense, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import styles from "./ProductPage.module.scss"
 import { doc, getDoc, getDocs, collection } from 'firebase/firestore';
@@ -12,24 +12,27 @@ const ProductPage = () => {
   const setActiveProduct = useProductsStore((state) => state.setActiveProduct)
   const activeProduct = useProductsStore((state) => state.activeProduct)
   const products = useProductsStore((state) => state.products)
-  console.log(products);
   const getSetProduct = async () => {
-    const displayProduct = products.find((product) => product.id === id)
-    console.log(displayProduct)
-    setActiveProduct(displayProduct)
+    const displayProduct = await getDoc(doc(db, "products", id))
+    const productPriceQuerySnapshot = await getDocs(collection(db, "products", id, "prices"))
+    productPriceQuerySnapshot.forEach((doc) => {
+      setActiveProduct({ ...displayProduct.data(), unit_amount: doc.data().unit_amount })
+    })
   }
   useEffect(() => {
     getSetProduct();
   }, [products])
 
   return (
-    <div className={styles.displayProductContainer}>
-      <img className={styles.displayImg} src={activeProduct.images} />
-      <h1 className={styles.displayProductHeader}>{activeProduct.name}</h1>
-      <p className={styles.displayProductDescription}>{activeProduct.description}</p>
-      <p className={styles.displayProductPrice}>${activeProduct.unit_amount / 100}</p>
-      <ProductCartButton product={activeProduct} />
-    </div>
+    <Suspense fallback={null}>
+      <div className={styles.displayProductContainer}>
+        <img className={styles.displayImg} src={activeProduct.images} />
+        <h1 className={styles.displayProductHeader}>{activeProduct.name}</h1>
+        <p className={styles.displayProductDescription}>{activeProduct.description}</p>
+        <p className={styles.displayProductPrice}>${activeProduct.unit_amount / 100}</p>
+        <ProductCartButton product={activeProduct} />
+      </div >
+    </Suspense>
   )
 }
 
