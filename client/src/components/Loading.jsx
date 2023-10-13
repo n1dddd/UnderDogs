ac589b85a6f261fb9bdeb55ba689a51de55ca6d8
Daniel <danielseniv@outlook.com>
2023-10-12 23:47:24 -0400

    Initialize loading component and scss module

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import loadingGif from "../assets/loading_1_500x500.gif"
+
+const Loading = () => {
+    return (
+        <img src={loadingGif} />
+    )
+}
+
+export default Loading
