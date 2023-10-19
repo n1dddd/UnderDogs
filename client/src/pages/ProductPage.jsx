a96d59f280bf434bc0168cb1885d9b1e0e6bde0d
Daniel <danielseniv@outlook.com>
2023-10-18 22:58:46 -0400

    Fix a bug where displayProduct id wasnt being passed through, allowing duplicate cart items of the same product. Add check for when loading state should trigger

--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -4,27 +4,40 @@ import styles from "./ProductPage.module.scss"
 import { doc, getDoc, getDocs, collection } from 'firebase/firestore';
 import { db } from '../config/firebase';
 import { useProductsStore } from '../stores/productsStore';
-import CartButtonComponent from '../components/CartButtonComponent';
 import ProductCartButton from '../components/ProductCartButton';
+import Loading from '../components/Loading';
 
 const ProductPage = () => {
   const { id } = useParams();
   const setActiveProduct = useProductsStore((state) => state.setActiveProduct)
   const activeProduct = useProductsStore((state) => state.activeProduct)
-  const products = useProductsStore((state) => state.products)
   const getSetProduct = async () => {
     const displayProduct = await getDoc(doc(db, "products", id))
     const productPriceQuerySnapshot = await getDocs(collection(db, "products", id, "prices"))
     productPriceQuerySnapshot.forEach((doc) => {
-      setActiveProduct({ ...displayProduct.data(), unit_amount: doc.data().unit_amount })
+      setActiveProduct({ ...displayProduct.data(), unit_amount: doc.data().unit_amount, id: displayProduct.id })
     })
   }
   useEffect(() => {
+    setActiveProduct({})
     getSetProduct();
-  }, [products])
+  }, [])
 
-  return (
-    <Suspense fallback={null}>
+  if (isNaN(activeProduct.unit_amount)) {
+    return (
+      <Loading />
+    )
+  } else if (activeProduct.unit_amount === 0) {
+    return (
+      <div className={styles.displayProductContainer}>
+        <img className={styles.displayImg} src={activeProduct.images} />
+        <h1 className={styles.displayProductHeader}>{activeProduct.name}</h1>
+        <p className={styles.displayProductDescription}>{activeProduct.description}</p>
+        <p className={styles.displayProductPrice}>${activeProduct.unit_amount / 100}</p>
+      </div >
+    )
+  } else {
+    return (
       <div className={styles.displayProductContainer}>
         <img className={styles.displayImg} src={activeProduct.images} />
         <h1 className={styles.displayProductHeader}>{activeProduct.name}</h1>
@@ -32,8 +45,9 @@ const ProductPage = () => {
         <p className={styles.displayProductPrice}>${activeProduct.unit_amount / 100}</p>
         <ProductCartButton product={activeProduct} />
       </div >
-    </Suspense>
-  )
+    )
+  }
+
 }
 
 
