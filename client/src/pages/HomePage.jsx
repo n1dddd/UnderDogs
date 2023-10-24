c0ca75fe1fa1602ee4f29021ae4fa43e85d9176b
Daniel <danielseniv@outlook.com>
2023-10-23 23:41:53 -0400

    Scrap old design, follow redesign solution

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,13 +1,30 @@
 import styles from "./HomePage.module.scss"
 import happyFace from '../assets/highQualityHappy.png'
-import FolderIcon from "../components/FolderIcon"
-
+import ParticlesBackground from "../components/ParticlesBackground"
+import { useNavigate } from "react-router-dom"
+import Banner from "../components/Banner"
 const HomePage = () => {
+    const navigate = useNavigate();
+    const navigateToSignUp = () => {
+        navigate("/sign_up")
+    }
+    const navigateToLogin = () => {
+        navigate("/login")
+    }
     return (
-        <div className={styles.HomePageContainer}>
-            <img src={happyFace} className={styles.bgImg} />
-            <FolderIcon />
-        </div>
+        <>
+            <ParticlesBackground />
+            <div className={styles.HomePageContainer}>
+                <Banner />
+                <img src={happyFace} className={styles.bgImg} />
+                <section className={styles.websiteCategories}>
+                    <h1 className={styles.menuHeader} onClick={() => navigateToSignUp()}>Sign-Up</h1>
+                    <h1 className={styles.menuHeader} onClick={() => navigateToLogin()}>Login</h1>
+                    <h1 className={styles.menuHeader}>Products</h1>
+                    <h1 className={styles.menuHeader}>Connect</h1>
+                </section>
+            </div>
+        </>
     )
 }
 
