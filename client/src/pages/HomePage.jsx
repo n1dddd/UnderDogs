a1d5aff8274e52873e057e32a6d9b7f7766027da
Daniel <danielseniv@outlook.com>
2023-10-24 01:43:21 -0400

    Integrate Navbar component to homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,26 +3,16 @@ import happyFace from '../assets/highQualityHappy.png'
 import ParticlesBackground from "../components/ParticlesBackground"
 import { useNavigate } from "react-router-dom"
 import Banner from "../components/Banner"
+import Navbar from "../components/Navbar"
 const HomePage = () => {
-    const navigate = useNavigate();
-    const navigateToSignUp = () => {
-        navigate("/sign_up")
-    }
-    const navigateToLogin = () => {
-        navigate("/login")
-    }
+
     return (
         <>
             <ParticlesBackground />
             <div className={styles.HomePageContainer}>
                 <Banner />
+                <Navbar />
                 <img src={happyFace} className={styles.bgImg} />
-                <section className={styles.websiteCategories}>
-                    <h1 className={styles.menuHeader} onClick={() => navigateToSignUp()}>Sign-Up</h1>
-                    <h1 className={styles.menuHeader} onClick={() => navigateToLogin()}>Login</h1>
-                    <h1 className={styles.menuHeader}>Products</h1>
-                    <h1 className={styles.menuHeader}>Connect</h1>
-                </section>
             </div>
         </>
     )
