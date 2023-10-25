c3558783623b6c78aab109fb13d0778e648063eb
Daniel <danielseniv@outlook.com>
2023-10-25 00:10:32 -0400

    Add conditional rendering based on if user is logged in or not for navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,25 +1,52 @@
 import styles from "./Navbar.module.scss"
 import { useNavigate } from "react-router-dom";
-import user from "/icons/user.svg"
+import { useUserStore } from "../stores/userStore.js";
+import { UserAuth } from '../context/AuthContext';
+import user from "/icons/userprofile.svg"
+import logoutIcon from "/icons/logout.svg"
 const Navbar = () => {
+    const authUser = useUserStore((state) => state.user);
+    const { logout } = UserAuth();
     const navigate = useNavigate();
-    const navigateToSignUp = () => {
-        navigate("/sign_up")
+    const handleLogout = async () => {
+        try {
+            await logout();
+            navigate('/')
+            console.log("You are logged out")
+            console.log(authUser);
+        }
+        catch (e) {
+            console.log(e.message)
+        }
     }
-    const navigateToLogin = () => {
-        navigate("/login")
+
+    if (!authUser) {
+        return (
+            <section className={styles.websiteCategories}>
+                <div className={styles.leftSideMenuBar}>
+                    <p className={styles.menuHeader}>Products</p>
+                    <p className={styles.menuHeader}>Connect</p>
+                </div>
+                <div className={styles.rightSideMenuBar} onClick={() => navigate('/login')}>
+                    <p className={styles.menuHeader}>Login</p>
+                    <img className={styles.userIcon} src={user} />
+                </div>
+            </section>
+        )
+    } else if (authUser) {
+        return (
+            <section className={styles.websiteCategories}>
+                <div className={styles.leftSideMenuBar}>
+                    <p className={styles.menuHeader}>Products</p>
+                    <p className={styles.menuHeader}>Connect</p>
+                </div>
+                <div className={styles.rightSideMenuBar} onClick={() => handleLogout()}>
+                    <p className={styles.userStatusHeader}>Logout</p>
+                    <img className={styles.logoutIcon} src={logoutIcon} />
+                </div>
+            </section>
+        )
     }
-    return (
-        <section className={styles.websiteCategories}>
-            <div className={styles.leftSideMenuBar}>
-                <p className={styles.menuHeader}>Products</p>
-                <p className={styles.menuHeader}>Connect</p>
-            </div>
-            <div className={styles.rightSideMenuBar}>
-                <img className={styles.userIcon} src={user} />
-            </div>
-        </section>
-    )
 }
 
 export default Navbar
