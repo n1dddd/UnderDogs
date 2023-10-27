11f2292fe9b2cafdab8f23ca69d671c39fcc0f9e
Daniel <danielseniv@outlook.com>
2023-10-26 23:29:35 -0400

    Add route for ProductsPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import SignIn from './pages/SignIn';
 import SignUp from "./pages/SignUp";
 import { AuthContextProvider } from "../src/context/AuthContext"
 import ProductPage from './pages/ProductPage';
+import ProductsPage from './pages/ProductsPage';
 
 
 function App() {
@@ -48,6 +49,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<SignIn />} />
         <Route path="/sign_up" element={<SignUp />} />
+        <Route path="/products" element={<ProductsPage />} />
         <Route path="/products/:id" element={<ProductPage />} />
       </Routes>
     </AuthContextProvider>
