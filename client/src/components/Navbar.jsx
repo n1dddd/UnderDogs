525871acba7a569a315dd78c22572074aceaf7fd
Daniel <danielseniv@outlook.com>
2023-10-30 21:21:02 -0400

    Change section HTML elements to nav elements, import Dropdown component for products tab in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { useUserStore } from "../stores/userStore.js";
 import { UserAuth } from '../context/AuthContext';
 import user from "/icons/userprofile.svg"
 import logoutIcon from "/icons/logout.svg"
+import DropdownComponent from "./DropdownComponent";
 const Navbar = () => {
     const authUser = useUserStore((state) => state.user);
     const { logout } = UserAuth();
@@ -22,29 +23,29 @@ const Navbar = () => {
 
     if (!authUser) {
         return (
-            <section className={styles.websiteCategories}>
+            <nav className={styles.websiteCategories}>
                 <div className={styles.leftSideMenuBar}>
-                    <p className={styles.menuHeader} onClick={() => navigate('/products')}>Products</p>
+                    <DropdownComponent />
                     <p className={styles.menuHeader}>Connect</p>
                 </div>
                 <div className={styles.rightSideMenuBar} onClick={() => navigate('/login')}>
                     <p className={styles.menuHeader}>Login</p>
                     <img className={styles.userIcon} src={user} />
                 </div>
-            </section>
+            </nav >
         )
     } else if (authUser) {
         return (
-            <section className={styles.websiteCategories}>
+            <nav className={styles.websiteCategories}>
                 <div className={styles.leftSideMenuBar}>
-                    <p className={styles.menuHeader} onClick={() => navigate('/products')}>Products</p>
+                    <DropdownComponent />
                     <p className={styles.menuHeader}>Connect</p>
                 </div>
                 <div className={styles.rightSideMenuBar} onClick={() => handleLogout()}>
                     <p className={styles.userStatusHeader}>Logout</p>
                     <img className={styles.logoutIcon} src={logoutIcon} />
                 </div>
-            </section>
+            </nav>
         )
     }
 }
