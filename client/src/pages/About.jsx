9534a9e2307091e2535874ea4f18d1905cb9a7c9
Daniel <danielseniv@outlook.com>
2023-11-06 22:13:41 -0500

    Add the about page content and preliminary styling

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,34 @@
 import styles from './About.module.scss'
+import ParticlesBackground from '../components/ParticlesBackground'
+import Banner from '../components/Banner'
+import Navbar from '../components/Navbar'
+import aboutGif from "../assets/2d_ud.gif"
 
 const About = () => {
     return (
-        <div>About</div>
+        <>
+            <ParticlesBackground />
+            <div className={styles.headerContainer}>
+                <Banner />
+                <Navbar />
+            </div>
+            <div className={styles.aboutContainer}>
+                <div className={styles.contentContainer}>
+                    <h1 className={styles.aboutHeader}>ABOUT</h1>
+                    <h2 className={styles.aboutText}>
+                        UNDERDOG is a online producer collective.
+                        Our goal is to create high quality and unique kits to help you stand out in the ever
+                        changing sound of music production. In a world where musical trends shift like quicksand,
+                        our mission is crystal clear: to empower you with the tools you need to shine. We strive
+                        to innovate & inspire the next generation.
+                    </h2>
+                    <h2 className={styles.aboutText2}>
+                        We're more than just a producer collective; we're a mindset.
+                        UNDERDOGS, It's not just a name; it's who we are.</h2>
+                    <img className={styles.aboutGif} src={aboutGif} />
+                </div>
+            </div>
+        </>
     )
 }
 
