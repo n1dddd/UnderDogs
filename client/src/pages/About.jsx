6af19de2112cb94015387f4d3981e8a97facb243
Daniel <danielseniv@outlook.com>
2023-11-11 14:11:20 -0500

    Restructure how page container is rendered, wrap entire content in page container to fix mobile rendering issues

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -8,24 +8,26 @@ const About = () => {
     return (
         <>
             <ParticlesBackground />
-            <div className={styles.aboutContainer}>
-                <div className={styles.headerContainer}>
-                    <Banner />
-                    <Navbar />
-                </div>
-                <div className={styles.contentContainer}>
-                    <h1 className={styles.aboutHeader}>ABOUT</h1>
-                    <h2 className={styles.aboutText}>
-                        UNDERDOG is a online producer collective.
-                        Our goal is to create high quality and unique kits to help you stand out in the ever
-                        changing sound of music production. In a world where musical trends shift like quicksand,
-                        our mission is crystal clear: to empower you with the tools you need to shine. We strive
-                        to innovate & inspire the next generation.
-                    </h2>
-                    <h2 className={styles.aboutText2}>
-                        We&apos;re more than just a producer collective; we&apos;re a mindset.
-                        UNDERDOGS, It&apos;s not just a name; it&apos;s who we are.</h2>
-                    <img className={styles.aboutGif} src={aboutGif} />
+            <div className={styles.aboutPageContainer}>
+                <div className={styles.aboutContentContainer}>
+                    <div className={styles.headerContainer}>
+                        <Banner />
+                        <Navbar />
+                    </div>
+                    <div className={styles.contentContainer}>
+                        <h1 className={styles.aboutHeader}>ABOUT</h1>
+                        <h2 className={styles.aboutText}>
+                            UNDERDOG is a online producer collective.
+                            Our goal is to create high quality and unique kits to help you stand out in the ever
+                            changing sound of music production. In a world where musical trends shift like quicksand,
+                            our mission is crystal clear: to empower you with the tools you need to shine. We strive
+                            to innovate & inspire the next generation.
+                        </h2>
+                        <h2 className={styles.aboutText2}>
+                            We&apos;re more than just a producer collective; we&apos;re a mindset.
+                            UNDERDOGS, It&apos;s not just a name; it&apos;s who we are.</h2>
+                        <img className={styles.aboutGif} src={aboutGif} />
+                    </div>
                 </div>
             </div>
         </>
