944a2923aee03f887bec7cdc9f973fedbfe9cac2
Daniel <danielseniv@outlook.com>
2023-11-14 23:26:40 -0500

    Initialize cart page and its styling

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { useCartStore } from '../stores/cartStore';
+import ParticlesBackground from '../components/ParticlesBackground';
+import Banner from '../components/Banner';
+import Navbar from '../components/Navbar';
 import styles from "./CartPage.module.scss";
 
 const CartPage = () => {
+    const statefulCart = useCartStore((state) => state.cart)
+    console.log(statefulCart)
     return (
-        <div>CartPage</div>
+        <div className={styles.cartPageContainer}>
+            <ParticlesBackground />
+            <div className={styles.headerContainer}>
+                <Banner />
+                <Navbar />
+            </div>
+            <section className={styles.cartPageContent}>
+                <div className={styles.cartItems}>
+
+                </div>
+                <div className={styles.cartInfo}>
+
+                </div>
+            </section>
+        </div>
     )
 }
 
