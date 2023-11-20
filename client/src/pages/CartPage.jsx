45a92a60830748a7968109304e8fae86952d72b1
Daniel <danielseniv@outlook.com>
2023-11-19 21:09:54 -0500

    Integrate cart page and cart view

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -4,10 +4,24 @@ import ParticlesBackground from '../components/ParticlesBackground';
 import Banner from '../components/Banner';
 import Navbar from '../components/Navbar';
 import styles from "./CartPage.module.scss";
+import XSymbol from "../assets/XSymbol.svg"
 
 const CartPage = () => {
     const statefulCart = useCartStore((state) => state.cart)
     console.log(statefulCart)
+    const removeFromCart = useCartStore((state) => state.removeFromCart);
+
+    const productsInCart = statefulCart.map((product, index) => {
+        return (
+            <div key={index} className={styles.checkoutContainer}>
+                <img className={styles.productFileIcon} src={product.product.images[0]} />
+                <p className={styles.productName}>{product.product.name}</p>
+                <p className={styles.productPrice}>${product.product.unit_amount / 100}</p>
+                <img className={styles.removeX} src={XSymbol} onClick={() => removeFromCart(product)} onTouchStart={() => removeFromCart(product)} />
+            </div>
+        )
+    })
+
     return (
         <div className={styles.cartPageContainer}>
             <ParticlesBackground />
@@ -16,9 +30,7 @@ const CartPage = () => {
                 <Navbar />
             </div>
             <section className={styles.cartPageContent}>
-                <div className={styles.cartItems}>
-
-                </div>
+                {[productsInCart]}
                 <div className={styles.cartInfo}>
 
                 </div>
